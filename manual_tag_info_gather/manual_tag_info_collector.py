8aa164a61a340674da958d684eae08ae09d18b74
sara-02 <sarahmasud02@gamil.com>
2018-02-16 11:29:10 +0530

    Update script as per Wildfly

--- manual_tag_info_gather/manual_tag_info_collector.py
+++ manual_tag_info_gather/manual_tag_info_collector.py
@@ -20,6 +20,7 @@ user_names = config.user_names
 prefix_url = config.prefix_url
 ecosystem = config.ecosystem
 Authorization = config.Authorization
+tag_prune_limit = int(config.tag_prune_limit)
 
 header = {'Authorization': Authorization}
 user_data_not_found = []
@@ -27,23 +28,20 @@ final_data_list = []
 for_each_ecosystem = {'ecosystem': ecosystem}
 final_package_dict = {}
 
-# For each user, collect the package_topic_map list
-try:
-    for name in user_names:
-        url = prefix_url + name + "/" + ecosystem
-        response = requests.get(url, headers=header)
-        if response.status_code != 200:
-            user_data_not_found.append(name)
-            continue
-        response_data = json.loads(response.content)
-        package_topic_map = response_data.get(
-            'data', {}).get('package_topic_map', {})
-        for package_name, tag_list in package_topic_map.items():
-            if package_name in final_package_dict.keys():
-                tag_list = tag_list.extend(final_package_dict[package_name])
-            final_package_dict[package_name] = list(set(tag_list))
-except Exception as e:
-    raise e
+for name in user_names:
+    url = prefix_url + name + "/" + ecosystem
+    response = requests.get(url, headers=header)
+    if response.status_code != 200:
+        user_data_not_found.append(name)
+        continue
+    response_data = json.loads(response.content)
+    package_topic_map = response_data.get(
+        'data', {}).get('package_topic_map', {})
+    for package_name, tag_list in package_topic_map.items():
+        if package_name in final_package_dict.keys():
+            tag_list = tag_list.extend(final_package_dict[package_name])
+        final_package_dict[package_name] = list(set(tag_list))
+
 
 if len(user_data_not_found) == len(user_names):
     print("Request unsuccessfull for all users")
@@ -52,12 +50,11 @@ if len(user_data_not_found) == len(user_names):
 if len(user_data_not_found) > 0:
     print("Request unsuccessfull for following users %s" % user_data_not_found)
 
-
 for_each_ecosystem['package_topic_map'] = final_package_dict
-final_list.append(for_each_ecosystem)
+final_data_list.append(for_each_ecosystem)
 
 with open('package_topic.json', 'w') as f:
-    json.dump(final_list, f, indent=4)
+    json.dump(final_data_list, f, indent=4)
 
 empty_count = 0  # No tags found.
 need_prune_count = 0  # Need to prune tag list length to 4.
@@ -66,7 +63,7 @@ for package in final_package_dict:
     plen = len(final_package_dict[package])
     if plen == 0:
         empty_count += 1
-    elif plen > 4:
+    elif plen > tag_prune_limit:
         need_prune_count += 1
 
 print("Total packages to tag in %s = %d" %
